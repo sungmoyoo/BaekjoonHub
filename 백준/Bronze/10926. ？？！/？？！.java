a035f215faaa223830f696c2e4fc40099d1ee0d9
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2023-12-30 18:08:13 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner keyScan = new Scanner(System.in);
+    String ID = keyScan.nextLine();
+    System.out.println(ID + "??!");
+  }
+}
