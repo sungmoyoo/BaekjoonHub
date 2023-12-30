36efa0622746485bbccfcce9e99c5d919623d77c
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2023-12-30 18:08:33 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 216 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner keyScan = new Scanner(System.in);
+    int y = keyScan.nextInt();
+    System.out.println(y - 543);
+    keyScan.close();
+  }
+}
