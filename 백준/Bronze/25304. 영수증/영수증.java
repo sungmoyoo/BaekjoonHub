207a58f5fb2cb815b08fe15c56d32671e6f10c65
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2023-12-30 18:15:05 +0900

    [Bronze IV] Title: 영수증, Time: 224 ms, Memory: 17820 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int X = sc.nextInt();
+    int N = sc.nextInt();
+    int result = 0;
+    
+    for (int i = 0; i < N; i++) {
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      result += (a*b);
+    }
+    System.out.println(result == X ? "Yes" : "No" );
+    sc.close();
+  }
+}
