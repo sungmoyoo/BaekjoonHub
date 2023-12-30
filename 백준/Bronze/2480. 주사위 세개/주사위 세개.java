49a5663c4ebeabf30802f8467a70cc7ff9fd8af3
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2023-12-30 18:13:43 +0900

    [Bronze IV] Title: 주사위 세개, Time: 212 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int dice1 = sc.nextInt();
+    int dice2 = sc.nextInt();
+    int dice3 = sc.nextInt();
+    int maxNumber;
+   
+    if (dice1 == dice2 && dice2 == dice3) {
+      System.out.println(10000 + dice1 * 1000);
+    } else if(dice1 == dice2 && dice1 != dice3) {
+      System.out.println(1000 + dice1 * 100);
+    } else if(dice2 == dice3 && dice1 != dice2) {
+      System.out.println(1000 + dice2 * 100);
+    } else if(dice1 == dice3 && dice1 != dice2) {
+      System.out.println(1000 + dice1 * 100);
+    } else {
+      maxNumber = Math.max(dice1, dice2);
+      maxNumber = Math.max(dice3, maxNumber);
+      System.out.println(100 * maxNumber);
+    }
+  }
+}
