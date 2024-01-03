8ce8318d339f7af7abef77b99c4e731b2b556a38
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-01-03 09:15:34 +0900

    [Bronze III] Title: 최소, 최대, Time: 512 ms, Memory: 92176 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      
+      int N = Integer.parseInt(br.readLine());
+      
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      
+      int[] arr = new int[N];
+      
+      for (int i = 0; i < N; i++) {
+        arr[i] = Integer.parseInt(st.nextToken());
+      }
+      
+      int min = arr[0];
+      int max = arr[0];
+      
+      for (int i : arr) {
+        min = Math.min(min, i);
+        max = Math.max(max, i);
+      }
+      
+      bw.write(min + " " + max);
+      
+      bw.close();
+      br.close();
+    }
+}
