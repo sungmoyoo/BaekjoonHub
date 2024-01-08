7d79c03d5f6c1f43af3781895934c8f36b94aedf
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-01-08 11:49:24 +0900

    [Bronze II] Title: 나머지, Time: 140 ms, Memory: 15832 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+// ArrayList contains을 활용한 방식
+//    int[] arr = new int[10];
+//    ArrayList<Integer> list = new ArrayList<>();
+//    for (int i = 0; i < 10; i++) {
+//      int n = Integer.parseInt(br.readLine());
+//      if (!list.contains(n % 42)) {
+//        list.add(n % 42);
+//      }
+//    }
+//    bw.write(list.size() + "\n");
+    
+    
+// HashSet을 활용한 방식
+    Set<Integer> set = new HashSet<>();
+    
+    for (int i = 0; i < 10; i++) {
+      int n = Integer.parseInt(br.readLine());
+      set.add(n % 42);
+    }
+    
+    bw.write(set.size() + "\n");
+    
+    bw.close();
+    br.close();
+  }
+}
