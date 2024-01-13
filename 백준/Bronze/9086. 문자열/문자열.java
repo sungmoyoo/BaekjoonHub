bebc800fe1515edfddd9ace69bd5cebaab284254
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-01-13 14:34:57 +0900

    [Bronze V] Title: 문자열, Time: 132 ms, Memory: 14584 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    int T = Integer.parseInt(br.readLine());
+    
+    for (int i = 0; i < T; i++) {
+      String str = br.readLine();
+      bw.write(str.charAt(0));
+      bw.write(str.charAt(str.length()-1) + "\n");
+    }
+    
+    bw.close();
+    br.close();
+  }
+}
