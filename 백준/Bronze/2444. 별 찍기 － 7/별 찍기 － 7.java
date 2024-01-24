a04ea996148e67a876985fa7af0bf0ac7b921e71
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-01-24 20:25:51 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 560 ms, Memory: 24560 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = (sc.nextInt()) * 2 - 1;
+
+    // 위의 정삼각형 (밑변n)
+    for (int i = 1; i <= n; i+=2) {
+      for (int j = 0; j < (n - i) / 2; j++) {
+        System.out.print(" ");
+      }
+      for (int k = 0; k < i ; k++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    } 
+
+
+    // 아래 정삼각형 (밑변n-2)
+    for (int i = n-2; i >= 1; i-=2) {
+      for (int j = 0; j < (n-i)/2; j++) {
+        System.out.print(" ");
+      }
+      for (int k = 0; k < i; k++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+    sc.close();
+  }
+}
