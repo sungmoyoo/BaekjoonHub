5e024d77c4cc8700207a28ceabf5e82f5a199b02
sungmoyoo <150773948+sungmoyoo@users.noreply.github.com>
2024-04-24 10:05:02 +0900

    [Bronze II] Title: 벌집, Time: 204 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int n = 0;
+    int sum = 1;
+
+    while (true) {
+      sum += (6*n);
+      n++;
+      if (N <= sum) {
+        break;
+      }
+    }
+
+    System.out.println(n);
+  }
+}
