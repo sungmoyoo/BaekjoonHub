c4a7354cf97b68bb5bb906f8ffa6fcd605fc6d42
sungmoyoo <samyoo644@gmail.com>
2025-01-18 23:12:46 +0900

    [Silver V] Title: 숫자 카드, Time: 424 ms, Memory: 121096 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,22 +1,10 @@
 import sys
 
 N = int(sys.stdin.readline())
-N_arr = list(map(int, sys.stdin.readline().split()))
+N_arr = set(map(int, sys.stdin.readline().split()))
 M = int(sys.stdin.readline())
 M_arr = list(map(int, sys.stdin.readline().split()))
 
-#교집합 찾기
-intersection_arr = set(N_arr) & set(M_arr)
+answer = [1 if x in N_arr else 0 for x in M_arr]
 
-# M 카드 딕셔너리 생성
-card_check = {card: 0 for card in M_arr}
-
-# 값 입력
-for n in intersection_arr:
-    card_check[n] = 1
-
-# 리스트로 변환
-answer = [card_check[x] for x in card_check]
-
-# 출력
 print(' '.join(map(str,answer)))
