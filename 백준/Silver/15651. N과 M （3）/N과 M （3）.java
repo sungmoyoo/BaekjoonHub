652c19887bc6d1bd2935cc559796c206897a1466
sungmoyoo <samyoo644@gmail.com>
2025-03-07 22:20:52 +0900

    [Silver III] Title: N과 M (3), Time: 408 ms, Memory: 68636 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+  static int[] arr;
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+    arr = new int[M];
+
+    backtracking(N, M, 0);
+
+    bw.write(new String(sb));
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+  public static void backtracking(int n, int m, int depth) {
+    if (depth == m) {
+      for (int num : arr) {
+        sb.append(num).append(" ");
+      }
+      sb.append("\n");
+      return;
+    }
+
+
+    for (int i = 1; i <= n; i++) {
+      arr[depth] = i;
+      backtracking(n, m, depth+1);
+    }
+  }
+}
+
+
+
