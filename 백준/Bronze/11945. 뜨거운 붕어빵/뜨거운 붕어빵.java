894f265aa2ca01b6086cc9efa38e2ab8df43e0d7
sungmoyoo <samyoo644@gmail.com>
2025-07-27 21:41:03 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+
+    char[][] bread = new char[N][M];
+
+    for (int i = 0; i < N; i++) {
+        bread[i] = br.readLine().toCharArray();
+    }
+
+    for (char[] n : bread) {
+      for (int i = n.length - 1; i >=0; i--) {
+        bw.write(n[i]);
+      }
+      bw.newLine();
+    }
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
