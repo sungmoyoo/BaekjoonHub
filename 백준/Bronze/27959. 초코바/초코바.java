b335f2d35e0c6cbc1525ebee08877f5ddc683dce
sungmoyoo <samyoo644@gmail.com>
2025-07-28 22:18:20 +0900

    [Bronze V] Title: 초코바, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+
+    if (N * 100 < M) {
+      bw.write("No");
+    } else {
+      bw.write("Yes");
+    }
+
+    bw.newLine();
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
