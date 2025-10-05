3428d83eca3943204cc724e95073764028563f9c
sungmoyoo <samyoo644@gmail.com>
2025-10-05 21:30:34 +0900

    [Bronze IV] Title: 한글 2, Time: 120 ms, Memory: 15908 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    char input = br.readLine().charAt(0);
+    int result = (input- '가') + 1;
+    bw.write(result + "\n");
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
