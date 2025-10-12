e239d7210b3c8f1ef0c164c8f67f96d1c6f3bc4f
sungmoyoo <samyoo644@gmail.com>
2025-10-12 22:56:33 +0900

    [Bronze V] Title: 라면 공식, Time: 116 ms, Memory: 15904 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int N = Integer.parseInt(br.readLine());
+
+    while (N-- > 0) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int A = Integer.parseInt(st.nextToken());
+      int B = Integer.parseInt(st.nextToken());
+      int X = Integer.parseInt(st.nextToken());
+
+      int W = A * (X-1) + B;
+
+      bw.write(W + "\n");
+    }
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
