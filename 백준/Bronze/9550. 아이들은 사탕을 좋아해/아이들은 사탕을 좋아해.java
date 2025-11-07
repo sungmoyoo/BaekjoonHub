0d4c5926aa9344e9667d67c714a13b1c8fc1f967
sungmoyoo <samyoo644@gmail.com>
2025-11-07 21:53:15 +0900

    [Bronze III] Title: 아이들은 사탕을 좋아해, Time: 136 ms, Memory: 14916 KB -BaekjoonHub

---
+++ 백준/Bronze/9550. 아이들은 사탕을 좋아해/아이들은 사탕을 좋아해.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int T = Integer.parseInt(br.readLine());
+
+    while (T-- > 0) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int N = Integer.parseInt(st.nextToken());
+      int K = Integer.parseInt(st.nextToken());
+      int result = 0;
+
+      for (int i = 0; i < N; i++) {
+        if (!st.hasMoreTokens()) {
+          st = new StringTokenizer(br.readLine());
+        }
+        result += Integer.parseInt(st.nextToken()) / K;
+      }
+
+
+      bw.write(result + "\n");
+    }
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
