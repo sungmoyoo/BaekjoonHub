3d871298b1fb248636ce0c316d3d05e4ea15fcc5
sungmoyoo <samyoo644@gmail.com>
2025-11-22 22:03:47 +0900

    [Bronze IV] Title: 치킨댄스를 추는 곰곰이를 본 임스 2, Time: 128 ms, Memory: 16028 KB -BaekjoonHub

---
+++ 백준/Bronze/26068. 치킨댄스를 추는 곰곰이를 본 임스 2/치킨댄스를 추는 곰곰이를 본 임스 2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int N = Integer.parseInt(br.readLine());
+    int count = 0;
+
+    while (N-- > 0) {
+      String[] gift = br.readLine().split("-");
+      if (Integer.parseInt(gift[1]) <= 90) {
+        count++;
+      }
+    }
+
+    bw.write(count + "\n");
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
