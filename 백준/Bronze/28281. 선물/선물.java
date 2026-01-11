b411e7110f8c38f02fb0f41ef784fbb7172aa30c
sungmoyoo <samyoo644@gmail.com>
2026-01-11 21:26:22 +0900

    [Bronze IV] Title: 선물, Time: 276 ms, Memory: 25268 KB -BaekjoonHub

---
+++ 백준/Bronze/28281. 선물/선물.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int X = Integer.parseInt(st.nextToken());
+
+    st = new StringTokenizer(br.readLine());
+    int[] price = new int[N];
+    for (int i = 0; i < N; i++) {
+      price[i] = Integer.parseInt(st.nextToken());
+    }
+
+    int min = Integer.MAX_VALUE;
+    for (int i = 1; i < N; i++) {
+      int totalPrice = X * (price[i-1] + price[i]);
+      min = Math.min(min, totalPrice);
+    }
+
+    bw.write(min + "\n");
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
