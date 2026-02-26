92f4affbf22e3e308b326f5a486634c9758ab0b6
sungmoyoo <samyoo644@gmail.com>
2026-02-26 22:05:53 +0900

    [Bronze IV] Title: 코드마스터 2024, Time: 116 ms, Memory: 14580 KB -BaekjoonHub

---
+++ 백준/Bronze/32215. 코드마스터 2024/코드마스터 2024.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+    int k = Integer.parseInt(st.nextToken());
+    
+    bw.write((k * m) + m + "\n");
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
