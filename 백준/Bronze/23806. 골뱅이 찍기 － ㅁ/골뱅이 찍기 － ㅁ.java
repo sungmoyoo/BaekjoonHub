0386fc6fd6768f1b55d7cbf3919e200d50c8318a
sungmoyoo <samyoo644@gmail.com>
2026-03-09 22:04:07 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅁ, Time: 168 ms, Memory: 15960 KB -BaekjoonHub

---
+++ 백준/Bronze/23806. 골뱅이 찍기 － ㅁ/골뱅이 찍기 － ㅁ.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int N = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < N * 5; i++) {
+      for (int j = 0; j < N * 5; j++) {
+        if (i < N || i >= N * 5 - N || j < N || j >= N * 5 - N) {
+          bw.write("@");
+        } else {
+          bw.write(" ");
+        }
+      }
+      bw.write("\n");
+    }
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
