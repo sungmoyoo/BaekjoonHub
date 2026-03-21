f73c454e2ef996ea3c722096aed2d87192eebca1
sungmoyoo <samyoo644@gmail.com>
2026-03-21 22:03:11 +0900

    [Bronze III] Title: 오버플로우와 모듈러, Time: 124 ms, Memory: 15768 KB -BaekjoonHub

---
+++ 백준/Bronze/15818. 오버플로우와 모듈러/오버플로우와 모듈러.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    long M = Long.parseLong(st.nextToken());
+
+    long result = 1;
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      result *= Long.parseLong(st.nextToken()) % M;
+      result %= M;
+    }
+    
+    bw.write(result + "\n");
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
