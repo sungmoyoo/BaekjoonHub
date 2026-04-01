1452fc3fce54a3b98c890794ae69d8a6fbe8c5e3
sungmoyoo <samyoo644@gmail.com>
2026-04-01 21:37:36 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㄹ, Time: 164 ms, Memory: 16084 KB -BaekjoonHub

---
+++ 백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int N = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < N * 5; i++) {
+      for (int j = 0; j < N * 5; j++) {
+        if (j < N ||
+            (j >= N * 2 && j < N * 3) ||
+            j >= N * 5 - N ||
+            (i < N && j < N * 2) ||
+            (i >= N * 5 - N && j >= N * 3)) {
+          bw.write("@");
+        } else {
+          bw.write(" ");
+        }
+      }
+      bw.write("\n");
+    }
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
