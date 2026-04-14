5c9a3343b8f40cea4b9b9d4b837f4648488ae7d5
sungmoyoo <samyoo644@gmail.com>
2026-04-14 21:48:46 +0900

    [Bronze III] Title: 단원평가, Time: 120 ms, Memory: 14504 KB -BaekjoonHub

---
+++ 백준/Bronze/31798. 단원평가/단원평가.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int a = Integer.parseInt(st.nextToken());
+    int b = Integer.parseInt(st.nextToken());
+    int c = Integer.parseInt(st.nextToken());
+
+    int result;
+    if (c == 0) {
+      result = (int) Math.sqrt(a+b);
+    } else {
+      result = (int) Math.pow(c, 2) - (a+b);
+    }
+
+    bw.write(result + "\n");
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
